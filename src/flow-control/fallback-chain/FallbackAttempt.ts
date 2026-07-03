f5b5a7bb632c422c8d094ea6978b0685a74da94c
Hasko <javics86@gmail.com>
2026-07-03 17:01:20 +0200

    fix(ci): 🐛 Align fallback attempt formatting

--- src/flow-control/fallback-chain/FallbackAttempt.ts
+++ src/flow-control/fallback-chain/FallbackAttempt.ts
@@ -1,10 +1,7 @@
 export class FallbackAttempt<T> {
   public constructor(
     private readonly attempt: () =>
-      | Promise<T | null | undefined>
-      | T
-      | null
-      | undefined,
+      Promise<T | null | undefined> | T | null | undefined,
   ) {}
 
   public async run(): Promise<T | null | undefined> {
